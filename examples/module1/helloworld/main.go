05b9ad70cafa1ce7eacd4686fbc51f9bc6359e9f
pengshuaijie <386692858@qq.com>
2021-10-19 22:40:39 +0800

    httpserver

--- examples/module1/helloworld/main.go
+++ examples/module1/helloworld/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	name := flag.String("name", "world", "specify the name you want to say hi")
+	name := flag.String("name", "psj", "specify the name you want to say hi")
 	flag.Parse()
 	fmt.Println("os args is:", os.Args)
 	fmt.Println("input parameter is:", *name)
--- httpserver/main.go
+++ httpserver/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	_ "os"
 
 	_ "net/http/pprof"
 
@@ -13,20 +14,10 @@ import (
 )
 
 func main() {
-	flag.Set("v", "4")
 	glog.V(2).Info("Starting http server...")
 	http.HandleFunc("/", rootHandler)
-	c, python, java := true, false, "no!"
-	fmt.Println(c, python, java)
+	http.HandleFunc("/healthz", healthz)
 	err := http.ListenAndServe(":80", nil)
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/", rootHandler)
-	// mux.HandleFunc("/healthz", healthz)
-	// mux.HandleFunc("/debug/pprof/", pprof.Index)
-	// mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	// mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	// mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	// err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,8 +28,17 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "ok\n")
 }
 
+func copyHeader(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			w.Header().Set(name, h)
+		}
+	}
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("entering root handler")
+	copyHeader(w, r)
 	user := r.URL.Query().Get("user")
 	if user != "" {
 		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
@@ -46,7 +46,10 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "hello [stranger]\n")
 	}
 	io.WriteString(w, "===================Details of the http request header:============\n")
+	r.Header.Set("VERSION", os.Getenv("VERSION"))
 	for k, v := range r.Header {
 		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
 	}
+	fmt.Println("client ip is:", r.RemoteAddr)
+	fmt.Println("Status code is:", w.Header().Get("Status-Code"))
 }
